ca4b41196323514ef517476ec0595dcb6103930a
Jesús Rodríguez Vargas <jrodriguez.vargas@atsistemas.com>
2023-03-30 21:13:43 +0200

    Primera subida

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InfoUsuarioComponent } from './components/info-usuario/info-usuario.component';
+import { TablaResultadoComponent } from './components/tabla-resultado/tabla-resultado.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'usuarios', pathMatch: 'full'},
+  { path: 'usuarios', component: TablaResultadoComponent,},
+  { path: 'usuarios/:id', component: InfoUsuarioComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
